d03d8d666cc64992c3f9ec806a9b448e26d5ed75
aki_ra <a0323w@gmail.com>
2024-06-29 14:49:58 +0900

    [add] マス探索の基底クラス

---
+++ Assets/Scripts/Piece/Base/IPiece.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public interface IPiece
+{
+    public List<int[]> MovableArea { get; }
+
+    public void Initialize(PieceMovementSearch movementBase);
+
+    public List<int[]> SearchMovableArea(int row, int column);
+}
---
+++ Assets/Scripts/Piece/Data/PieceData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Piece", menuName = "ScriptableObjects/CreatePieceData")]
+public class PieceData : ScriptableObject
+{
+    [field: SerializeField]
+    public PieceType PieceType { get; private set; }
+    [field: SerializeField]
+    public CharacterType CharacterType { get; private set; }
+}
+
+public enum PieceType
+{
+    None = 0,
+}
+
+public enum CharacterType
+{
+    None,
+    Attacker,
+    Buffer,
+    Support,
+    Tank,
+}
