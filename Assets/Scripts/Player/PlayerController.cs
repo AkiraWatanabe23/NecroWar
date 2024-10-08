7ce7d5bcb14afaf3e5470e0c9e3f23678eba23eb
aki_ra <a0323w@gmail.com>
2024-10-08 18:07:27 +0900

    [add] 複数シーンの動的ロード、アンロード

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using Network;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Button _hostButton = default;
+    [SerializeField]
+    private Button _joinButton = default;
+
+    private NetworkController _networkController = default;
+
+    private void Start()
+    {
+        Register();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) { StartCoroutine(DropOut()); }
+    }
+
+    /// <summary> サーバーの立ち上げ処理 </summary>
+    private void Register()
+    {
+        if (_hostButton == null) { return; }
+
+        _hostButton.onClick.AddListener(() =>
+        {
+            _networkController ??= new NetworkController();
+            _networkController.Build();
+        });
+    }
+
+    /// <summary> サーバーシーンのアンロード </summary>
+    private IEnumerator DropOut()
+    {
+        if (_networkController == null) { yield break; }
+
+        yield return _networkController.UnLoad();
+
+        _networkController = null;
+        yield return null;
+    }
+
+    private void OnDestroy()
+    {
+        if (!gameObject.activeSelf) { return; }
+
+        StartCoroutine(DropOut());
+    }
+}
